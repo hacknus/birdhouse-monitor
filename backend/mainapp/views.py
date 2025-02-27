8264284e01182aedb6516ef1e57f663bb7cb0477
hacknus <linus.stoeckli@unibe.ch>
2025-02-27 23:01:54 +0100

    increase video size

--- backend/mainapp/views.py
+++ backend/mainapp/views.py
@@ -12,7 +12,7 @@ from picamera2 import Picamera2
 
 # init camera
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (128, 128)}))
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (800, 600)}))
 picam2.start()
 
 
