782be215bf2251d0a8c52f850976f37b6e291ed8
hacknus <linus.stoeckli@unibe.ch>
2025-02-27 22:17:54 +0100

    fix

--- backend/camera/views.py
+++ backend/camera/views.py
@@ -1,11 +1,8 @@
-# backend/camera/views.py
+from django.shortcuts import render
 
-from django.http import StreamingHttpResponse
-from livestream.stream import CameraStream
-
-# Initialize the camera stream object
-camera_stream = CameraStream()
-
-def video_feed(request):
-    """Django view to stream the video feed"""
-    return StreamingHttpResponse(camera_stream.gen(), content_type='multipart/x-mixed-replace; boundary=frame')
+# View for the camera stream page
+def index(request):
+    """
+    Renders the template for the live stream page.
+    """
+    return render(request, 'camera/index.html')
