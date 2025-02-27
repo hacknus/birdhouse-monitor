48346e63e1a2dbe41e0bfbae15b97e132d51e47f
hacknus <linus.stoeckli@unibe.ch>
2025-02-27 20:55:12 +0100

    try this

--- backend/camera/urls.py
+++ backend/camera/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import camera_home, capture_image, toggle_ir, stream_mjpg, stop_camera, get_gallery
+from .views import camera_home, capture_image, toggle_ir, stream_mjpg, stop_camera, get_gallery, start_stream
 
 app_name = 'camera'  # Add this line to define the app's namespace
 
 urlpatterns = [
     path("", camera_home, name="camera_home"),  # <-- Add this line
+    path('camera/start_stream/', start_stream, name='start_stream'),
     path("capture/", capture_image, name="capture_image"),  # Capture image
     path("toggle_ir/", toggle_ir, name="toggle_ir"),  # Toggle IR light
     path('stream.mjpg', stream_mjpg, name='stream_mjpg'),
