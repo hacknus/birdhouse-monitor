db37a545db51d27651f215f1099195b191a0e3c4
hacknus <linus.stoeckli@unibe.ch>
2025-02-27 22:49:23 +0100

    remove unused

--- backend/mainapp/urls.py
+++ backend/mainapp/urls.py
@@ -3,7 +3,7 @@ from mainapp import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("sensors", views.sensors, name="sensors"),
-    path("settings", views.settings, name="settings"),
+    # path("sensors", views.sensors, name="sensors"),
+    # path("settings", views.settings, name="settings"),
     path("video_feed", views.video_feed, name="video_feed"),
 ]
