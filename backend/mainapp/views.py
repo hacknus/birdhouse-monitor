8c54bfaf3ccf5185dc1d61da6ed20bc82b660aa7
hacknus <linus.stoeckli@unibe.ch>
2025-02-28 00:55:09 +0100

    fix import

--- backend/mainapp/views.py
+++ backend/mainapp/views.py
@@ -13,7 +13,7 @@ from django.shortcuts import render
 from picamera2 import Picamera2
 
 from .models import SensorData
-from .sensor_acquisition import *
+import backend.mainapp.sensor_acquisition
 
 # init camera
 picam2 = Picamera2()
