a777077b42ee6293df28bfc6aca2e15181fd2ec6
hacknus <linus.stoeckli@unibe.ch>
2025-02-28 00:57:59 +0100

    fix fields

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class SensorData(models.Model):
     temperature = models.FloatField()
     humidity = models.FloatField()
-    motion_triggered = models.BooleanField(default=False)
+    motion_triggered = models.BooleanField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Data at {self.timestamp}: {self.temperature}°C, {self.humidity}%"
+        return f"Sensor data at {self.timestamp}"
