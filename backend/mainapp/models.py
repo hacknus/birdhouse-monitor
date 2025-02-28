521b2b8f21fa8596c3515ded15f6ef50716605f7
hacknus <linus.stoeckli@unibe.ch>
2025-02-28 01:01:25 +0100

    fix

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class SensorData(models.Model):
     temperature = models.FloatField()
     humidity = models.FloatField()
-    motion_detected = models.BooleanField()
+    motion_triggered = models.BooleanField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.temperature}°C, {self.humidity}%, Motion: {self.motion_detected}"
+        return f"{self.temperature}°C, {self.humidity}%, Motion: {self.motion_triggered}"
