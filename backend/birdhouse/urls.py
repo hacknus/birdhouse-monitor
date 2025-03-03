7c83f9a94058aedeb759bab5ee1c170714468a29
hacknus <linus.stoeckli@unibe.ch>
2025-03-03 09:44:50 +0100

    fix static for cgnum/voegeli

--- backend/birdhouse/urls.py
+++ backend/birdhouse/urls.py
@@ -17,5 +17,5 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path("", include("mainapp.urls"))]
+urlpatterns = [path("voegeli/", include("mainapp.urls"))]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
