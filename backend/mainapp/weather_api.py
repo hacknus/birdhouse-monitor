f9bac242f20ea9d20fc0e4082e2eb3f121b9393e
hacknus <linus.stoeckli@unibe.ch>
2025-03-06 17:31:14 +0100

    hardcode this

--- backend/mainapp/weather_api.py
+++ backend/mainapp/weather_api.py
@@ -17,7 +17,7 @@ def get_access_token():
     # Encode the credentials in Base64
     credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"
     encoded_credentials = base64.b64encode(credentials.encode("utf-8")).decode("utf-8")
-    encoded_credentials[-1] = "="  # Remove any accidental newline characters
+    encoded_credentials = encoded_credentials[:-1] + "="
     print(encoded_credentials)
     # Define the API request
     url = "https://api.srgssr.ch/oauth/v1/accesstoken?grant_type=client_credentials"
