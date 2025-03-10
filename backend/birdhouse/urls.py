fc92e0a2695df59fdceee7ad232d057da2bc4cc8
hacknus <linus.stoeckli@unibe.ch>
2025-03-10 16:55:39 +0100

    forward to voegeli

--- backend/birdhouse/urls.py
+++ backend/birdhouse/urls.py
@@ -17,5 +17,9 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path("voegeli/", include("mainapp.urls"))]
+
+urlpatterns = [
+    path('', lambda request: redirect('/voegeli/', permanent=True)),  # Redirect root to /voegeli/
+    path("voegeli/", include("mainapp.urls")),
+]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
