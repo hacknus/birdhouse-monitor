45cbb13791e4133044408d63d35205637b971597
hacknus <linus.stoeckli@unibe.ch>
2025-03-13 10:55:32 +0100

    add auth

--- backend/birdhouse/settings.py
+++ backend/birdhouse/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
+    'django.contrib.auth',
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
