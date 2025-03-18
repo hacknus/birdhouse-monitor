84e68766e11b0e552446eb238d27f79d951c44b8
hacknus <linus.stoeckli@unibe.ch>
2025-03-18 07:22:04 +0100

    debug

--- backend/mainapp/middleware.py
+++ backend/mainapp/middleware.py
@@ -12,21 +12,26 @@ class TrackVisitorMiddleware:
             (request.META.get("REMOTE_ADDR", "") + request.META.get("HTTP_USER_AGENT", "")).encode()
         ).hexdigest()
 
+        print("Visitor key:", visitor_key)
+
         # Store visitor in cache with 5-minute expiration
         cache.set(visitor_key, now(), timeout=300)
 
         response = self.get_response(request)
+        print("Visitor response:", response)
         return response
 
 
 def get_active_visitors():
     from django.core.cache import caches
+    print("Getting active visitors...")
     cache_backend = caches["default"]
+    print("Cache backend:", cache_backend)
 
     if hasattr(cache_backend, "client"):  # Ensure Redis is being used
         redis_client = cache_backend.client.get_client(write=True)
         print(f"{redis_client=}")
         visitor_keys = list(redis_client.scan_iter("visitor_*"))
         return len(visitor_keys)
-
+    print("no redis")
     return 0  # If not using Redis, return 0 (or handle differently)
