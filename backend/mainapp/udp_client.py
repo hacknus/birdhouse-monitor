71de73dcf57013e2968c866af3385d5c61b60985
hacknus <linus.stoeckli@unibe.ch>
2025-04-01 00:26:30 +0200

    bind to raspberry ip

--- backend/mainapp/udp_client.py
+++ backend/mainapp/udp_client.py
@@ -31,7 +31,7 @@ class UDPVideoClient:
 
     def _receive_frames(self):
         while self.running:
-            ip = "0.0.0.0"
+            ip = self.read_ip()
             try:
                 print(f"[UDP Client] Binding to {ip}:{self.udp_port}")
                 self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
